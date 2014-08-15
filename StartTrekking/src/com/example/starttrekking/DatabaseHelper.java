e37070be2319f062eb2cdceebd50728e9569e454
lfsaestrella <lfsaestrella@gmail.com>
2014-08-15 15:30:26 -0300

    banco de dados

--- StartTrekking/src/com/example/starttrekking/DatabaseHelper.java
+++ StartTrekking/src/com/example/starttrekking/DatabaseHelper.java
@@ -7,22 +7,28 @@ import android.os.Environment;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 	 
-    private static final int DATABASE_VERSION = 2;
-    private static final String DATABASE_NAME = "db_st";
- 
-    DatabaseHelper (Context context) {
-    	super(context, Environment.getExternalStorageDirectory().getAbsolutePath() + "/start-trekking/database/" + DATABASE_NAME, null, DATABASE_VERSION);
-    }
+	private static DatabaseHelper mInstance = null;
 
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
-		
-	}
+	  private static final String DATABASE_NAME = "database_name";
+	  private static final String DATABASE_TABLE = "table_name";
+	  private static final int DATABASE_VERSION = 1;
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
-		
-	}
+	  public static DatabaseHelper getInstance(Context ctx) {
+
+	    // Use the application context, which will ensure that you 
+	    // don't accidentally leak an Activity's context.
+	    // See this article for more information: http://bit.ly/6LRzfx
+	    if (mInstance == null) {
+	      mInstance = new DatabaseHelper(ctx.getApplicationContext());
+	    }
+	    return mInstance;
+	  }
+
+	  /**
+	   * Constructor should be private to prevent direct instantiation.
+	   * make call to static factory method "getInstance()" instead.
+	   */
+	  private DatabaseHelper(Context ctx) {
+	    super(ctx, DATABASE_NAME, null, DATABASE_VERSION);
+	  }
 }
